e500a7a81cd7c8c5f206d14cdcaba7a38c8da0f5
Wilhelm Grosz <wilhelm.grosz@iquestgroup.com>
2019-02-22 22:39:02 +0200

    removes leftover ListComponent that shouldn't be added to the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 
 import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductsListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
