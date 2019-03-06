1ad7489c7ceee7042d910d0b115e63c0adee93e0
Wilhelm Grosz <wilhelm.grosz@iquestgroup.com>
2019-03-06 08:37:49 +0200

    fixes the transition addition so that the zoom is smoother

--- src/app/zoom.directive.ts
+++ src/app/zoom.directive.ts
@@ -16,13 +16,13 @@ export class ZoomImageDirective {
   }
 
   private addStyle() {
+    this.el.nativeElement.style['transition'] = 'transform 1s';
     this.el.nativeElement.style['transform'] = 'scale(1.3)';
     this.el.nativeElement.style['border'] = '1px solid green';
     this.el.nativeElement.style['z-index'] = 101;
   }
 
   private clearStyle() {
-    this.el.nativeElement.style['transition'] = 'transition: transform 1s;';
     this.el.nativeElement.style['transform'] = 'scale(1)';
     this.el.nativeElement.style['border'] = 'none';
     this.el.nativeElement.style['z-index'] = 100;
