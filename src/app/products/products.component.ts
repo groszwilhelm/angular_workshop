8de690d828317174d244d9fa300c659c514a3813
Marius Axinte <marius.axinte@iquestgroup.com>
2019-11-14 17:04:43 +0200

    Text sanitizer

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Subscription, interval, timer, fromEvent, Observable } from "rxjs";
+import { debounce, take, pluck, map, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: "wsh-products",
@@ -9,5 +10,23 @@ import { Subscription } from "rxjs";
 export class ProductsComponent implements OnInit {
   mySubscription: Subscription;
 
-  ngOnInit() {}
+  textStream$: Observable<string>;
+
+  ngOnInit() {
+
+    const badWords = ['bad', 'foo', 'bar'];
+
+    const input = document.querySelector('input')
+    const event$ = fromEvent(input, 'keyup');
+
+    this.textStream$ = event$.pipe(
+      debounceTime(500),
+      map((evt: any) => evt.target.value),
+      map(val => {
+        const chunks = val.split(' ');
+        return chunks.filter(word => !badWords.includes(word)).join(' ');
+      })
+    )
+    
+  }
 }
